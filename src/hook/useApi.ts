a6375af540e9c51790103ec61ef9ced401332577
Alex Navarro <alexnavter@gmail.com>
2023-02-19 13:45:56 +0100

    Create useApi hook

---
+++ src/hook/useApi.ts
@@ -0,0 +1,23 @@
+import { useCallback } from "react";
+import { loadRobotsActionCreator } from "../store/features/robots/robotsSlice";
+import { useAppDispatch } from "../store/hooks";
+
+const useApi = () => {
+  const dispatch = useAppDispatch();
+
+  const getRobots = useCallback(async () => {
+    try {
+      const response = await fetch(process.env.REACT_APP_URL_API!);
+
+      const robots = await response.json();
+
+      dispatch(loadRobotsActionCreator(robots.robots));
+    } catch (error) {
+      return (error as Error).message;
+    }
+  }, [dispatch]);
+
+  return { getRobots };
+};
+
+export default useApi;
