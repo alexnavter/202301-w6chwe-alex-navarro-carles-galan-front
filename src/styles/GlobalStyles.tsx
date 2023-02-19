7f663cd349e3baf3565ceb1d5953b4767ef4f49a
Alex Navarro <alexnavter@gmail.com>
2023-02-19 23:20:11 +0100

    Add global styles

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -1,18 +1,22 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
-  *, 
+*, 
 *:after,
 *:before {
+  margin: 0;
+  padding: 0;
   box-sizing: border-box;
   text-decoration: none;
-}
+  }
 
 body {
+  margin: 0;
   padding: 20px;
-  background-color: #fff;
-  font-family: helvetica;
+  background-color:#565A97;
+  width: 100%;
   min-height: 100vh;
+
 }
 
 ol, ul, li {
